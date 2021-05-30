1a2c6611819da7fd0f864b0884a62bb74b11adc4
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-30 14:04:07 +0530

    [Shreya]Add: Welcome to Employee Wage Problem

---
+++ EmployeeWageProblemUsingOops/EmployeeWageProblemUsingOops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblemUsingOops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.out.println("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
